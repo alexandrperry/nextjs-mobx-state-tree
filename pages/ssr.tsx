8c02c0f7489ed19418ad584c4736237a0da9fbdd
Alexandr Chernyakov <chernyakovit@gmail.com>
2020-06-08 02:57:17 +0300

    delete comments

--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -3,9 +3,8 @@ import { rootStore, RootInstance } from 'store';
 
 const Ssg: React.FC<RootInstance> = (props) => <p>{JSON.stringify(props)}</p>;
 
-// If you build and start the app, the date returned here will have the same
-// value for all requests, as this method gets executed at build time.
 export default Ssg;
+
 export async function getServerSideProps() {
   await rootStore.autoSearch.fetch('par');
   return { props: getSnapshot(rootStore) };
