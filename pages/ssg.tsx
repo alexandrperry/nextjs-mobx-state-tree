f08f70ad499f31c40c85d8ea91ff9fd9cddb7646
Alexandr Chernyakov <chernyakovit@gmail.com>
2020-06-08 02:57:10 +0300

    change logic

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -3,9 +3,8 @@ import { RootInstance, rootStore } from 'store';
 
 const Ssg: React.FC<RootInstance> = (props) => <p>{JSON.stringify(props)}</p>;
 
-// If you build and start the app, the date returned here will have the same
-// value for all requests, as this method gets executed at build time.
 export default Ssg;
+
 export async function getStaticProps() {
   await rootStore.autoSearch.fetch('barc');
 
--- pages/ssr.tsx
+++ pages/ssr.tsx
@@ -1,5 +1,5 @@
 import { getSnapshot } from 'mobx-state-tree';
-import { initializeStore, RootInstance } from 'store';
+import { rootStore, RootInstance } from 'store';
 
 const Ssg: React.FC<RootInstance> = (props) => <p>{JSON.stringify(props)}</p>;
 
@@ -7,9 +7,6 @@ const Ssg: React.FC<RootInstance> = (props) => <p>{JSON.stringify(props)}</p>;
 // value for all requests, as this method gets executed at build time.
 export default Ssg;
 export async function getServerSideProps() {
-  const store = initializeStore();
-
-  await store.autoSearch.fetch('par');
-
-  return { props: getSnapshot(store) };
+  await rootStore.autoSearch.fetch('par');
+  return { props: getSnapshot(rootStore) };
 }
