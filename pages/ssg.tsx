0485c8ea00cf8496bd658bb7eb07fc5a2bcf3767
Alexandr Chernyakov <chernyakovit@gmail.com>
2020-06-07 23:45:33 +0300

    change types

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,7 +1,7 @@
 import { getSnapshot } from 'mobx-state-tree';
-import { initializeStore } from 'store';
+import { initializeStore, RootInstance } from 'store';
 
-const Ssg = (props) => <p>{JSON.stringify(props)}</p>;
+const Ssg: React.FC<RootInstance> = (props) => <p>{JSON.stringify(props)}</p>;
 
 // If you build and start the app, the date returned here will have the same
 // value for all requests, as this method gets executed at build time.
